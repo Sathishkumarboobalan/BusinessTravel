f79561cd37c2a4b7abd0685638e7cb6c5469b426
Sathishkumarboobalan <39888634+Sathishkumarboobalan@users.noreply.github.com>
2018-06-05 00:10:47 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'eventPage.dart';
+import 'orderDetails.dart';
+import 'landingPage.dart';
+import 'tagPage.dart';
+import 'Preferences/main.dart';
+/*
+void main() {
+  runApp(
+    new MaterialApp(
+      home: new LandingPage(),
+      routes: <String, WidgetBuilder> {
+        '/landing' : (BuildContext context) => new LandingPage(),
+        '/OrderDetails': (BuildContext context) => new OrderDetails(),
+        '/Preferences' : (BuildContext context) => new Preferences(),
+        
+      },
+    )
+  );
+}*/
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  final routes =  <String, WidgetBuilder> {
+        LandingPage.tag : (BuildContext context) => new LandingPage(),
+        OrderDetails.tag: (BuildContext context) => new OrderDetails(),
+        MyPreferences.tag : (BuildContext context) => new MyPreferences(),
+        TagPage.tag : (BuildContext context) => new TagPage(),
+        EventListPage.tag: (BuildContext context) => new EventListPage(),
+      };
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp( 
+      title: "BusinessTravel",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.lightBlue ,
+    ),
+    routes: routes,
+    home: LandingPage(),
+    );
+  }
+}
